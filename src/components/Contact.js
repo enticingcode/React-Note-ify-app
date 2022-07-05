cec854693add011a82ffe3cd3f7245f10d1e7dd3
Marvin <trujillomarvin@hotmail.com>
2022-07-05 11:42:54 -0500

    going to be changing sidebar to notes name

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div>Contact</div>
+        <div className='contact--page'>
+            <h1>Reach out to us for improvement suggestions!</h1>
+            <form>
+                <div className="form--inputs">
+                    <input type='text' placeholder="First Name"></input>
+                    <input type='text' placeholder="Last Name"></input>
+                    <input type='text' placeholder="Phone"></input>
+                    <input type='text' placeholder="Email"></input>
+                    <textarea placeholder="Comments"></textarea>
+                </div>
+            </form>
+        </div>
     )
 }
 
