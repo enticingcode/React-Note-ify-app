e3a366ea7c89980201fee5eca88d63b06055eeaf
marvo <trujillomarvin@hotmail.com>
2022-07-14 14:21:53 -0500

    changed api

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,19 @@
 import { initializeApp } from "firebase/app";
 import "firebase/auth"
-const app = initializeApp()
+
+
+
+
+
+// Initialize Firebase
+const app = initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env,
+    projectId: "note-ify-be488",
+    storageBucket: "note-ify-be488.appspot.com",
+    messagingSenderId: "85174083605",
+    appId: "1:85174083605:web:1f49acd6364df0d42e2e1c",
+    measurementId: "G-3V2X478E4T"
+});
+
+const analytics = getAnalytics(app);
