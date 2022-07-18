058257693dd2703d243e6cd3fa4ecd5c7acd4514
Marvin <trujillomarvin@hotmail.com>
2022-07-18 14:46:24 -0500

    firebase commits

--- src/App.js
+++ src/App.js
@@ -57,6 +57,7 @@ export default function App() {
                 }
                 else newArray.push(oldNote);
             }
+            editSFnote(oldNote)
             return newArray
         })
     }
